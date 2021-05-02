e1e1ecd8e4099944bf430e7394e3255c6cd68582
Sharmin Akter <72566503+SharminAkter1@users.noreply.github.com>
2021-05-03 05:26:28 +0600

    complete

---
+++ app.js
@@ -0,0 +1,58 @@
+//control statement
+   // conditinnal control statement
+      //if, else if, else, switch
+     
+   // Loop Control statement
+     //for, while, do while
+     
+//control statement
+//  var num = 6;
+//  if (num % 2 == 0)
+//  console.log("Even");
+
+//  else (num %2 != 0)
+//  console.log("Odd");  
+
+// // Nagative and positive number
+//  var num = prompt('Enter a Number');
+
+//  if (num > 0)
+//  console.log('Positive');
+
+//  else if (num == 0)
+//  console.log ('Positive');
+
+//  else (num < 0)
+//  console.log ('Nagative');
+
+
+ // Letter Grade
+  var marks = prompt('Enter your marks : ');
+
+  if (marks >=80) {
+  console.log('A+');
+ }
+
+  else if (marks >=70){
+  console.log('A');
+ }
+
+  else if (marks >=60){
+  console.log('A-');
+ }
+
+  else if (marks >=50){
+  console.log('B');
+ }
+
+  else if (marks >=40){
+  console.log('C');
+ }
+
+  else if (marks >=33){
+  console.log('D');
+ }
+
+  else {
+    console.log('Fail');
+  }
